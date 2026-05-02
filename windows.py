226eb4447bb4ca91bcc1a9ce4ccd95edd8a9d350
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-05-02 10:52:19 +0100

    Add files via upload

---
+++ windows.py
@@ -0,0 +1,13 @@
+#change next 2 lines
+winas="c:\\MinGW\\bin\\as.exe"
+winobj="c:\\MinGW\\bin\\objdump.exe"
+import os
+txt=".intel_syntax noprefix\n"
+for a in range(1,10):
+    txt=txt+"a"+str(a)+":\n.ascii  \"" +(a*"*")+"\\0\"\n"
+f1=open("out.s","w")
+f1.write(txt)
+f1.close()
+os.system(winas+" --32 -o out.o out.s")
+os.system("cat out.o")
+os.system(winobj+" -x out.o")
