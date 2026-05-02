877764549130fdae806e6636938e4649208bd98c
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-05-02 11:06:20 +0100

    Change output format in windows.py

--- windows.py
+++ windows.py
@@ -4,7 +4,7 @@ winobj="c:\\MinGW\\bin\\objdump.exe"
 import os
 txt=".intel_syntax noprefix\n"
 for a in range(1,10):
-    txt=txt+"a"+str(a)+":\n.ascii  \"" +(a*"*")+"\\0\"\n"
+    txt=txt+"a"+str(a)+":\n.word " +str(a)+"\n"
 f1=open("out.s","w")
 f1.write(txt)
 f1.close()
